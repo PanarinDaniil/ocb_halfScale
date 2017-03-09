6c33a57f68a9d223b30a5cb59a48b9e1c45658c3
Dan Panarin <panarin.daniil@gmail.com>
2017-03-09 23:07:06 +0200

    footer html + _footer.scss

--- js/scripts.js
+++ js/scripts.js
@@ -20,4 +20,47 @@ $(document).ready(function(){
             cssEase: 'linear'
         });
     }
+    var counter = false;
+    if ( $(".gallery").width() > 0 ) {
+        if ( window.matchMedia("(max-width:800px)").matches && counter == false ) {
+            $('.gallery').slick({
+                lazyLoad: 'ondemand',
+                dots: true,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 10000,
+                infinite: true,
+                speed: 500,
+                fade: true,
+                cssEase: 'linear',
+                swipe: true,
+                swipeToSlide: true
+            });
+            counter = true;
+        } else if ( window.matchMedia("(min-width:801px)").matches && counter == true ) {
+            $('.gallery').slick('unslick');
+            counter = false;
+        }
+        $(window).resize(function() {
+            if( window.matchMedia("(max-width:800px)").matches && counter == false) {
+                $('.gallery').slick({
+                    lazyLoad: 'ondemand',
+                    dots: true,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 10000,
+                    infinite: true,
+                    speed: 500,
+                    fade: true,
+                    cssEase: 'linear',
+                    swipe: true,
+                    swipeToSlide: true
+                });  
+                counter = true;
+            } else if ( window.matchMedia("(min-width:801px)").matches && counter == true ) { // must be 17px less then in css
+                $('.gallery').slick('unslick');
+                counter = false;
+            }
+        });
+    }
 });
